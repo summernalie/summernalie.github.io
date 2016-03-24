acc461c90c9e417cd4bb098c7fdf1cd2610bdc5c
Samanalie <samanalie@outlook.com>
2016-03-24 21:54:20 +0530

    update header animation

--- theme1/assets/js/custom.js
+++ theme1/assets/js/custom.js
@@ -1,4 +1,4 @@
-﻿/*=============================================================
+/*=============================================================
     Authour URI: www.binarytheme.com
     License: Commons Attribution 3.0
 
@@ -50,6 +50,32 @@
 
     $(document).ready(function () {
         mainApp.main_fun();
+        
+        
+        window.addEventListener('scroll', function(e){
+            var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+                shrinkOn = 150,
+                header = document.querySelector("header");
+            if (distanceY > shrinkOn) {
+                $('#logo').addClass('shift');
+                $('#menu').addClass('shrink');
+                //$('#menu h1.hidden').removeClass('hidden');
+            } else {
+                $('#logo').removeClass('shift');
+                $('#menu').removeClass('shrink');
+                //$('#menu h1').addClass('hidden');
+            }
+        });
     });
+    
+    
+    /*$(window).on('scroll', function() {
+    var y_scroll_pos = window.pageYOffset;
+    var scroll_pos_test = 150;             // set to whatever you want it to be
+
+    if(y_scroll_pos > scroll_pos_test) {
+        $('#logo').addClass('shift');
+    }
+});*/
 
 }(jQuery));
